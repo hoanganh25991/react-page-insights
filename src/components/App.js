57ffd717dfdb6972f8955c1bf9693d51fe647340
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-12-19 14:13:06 +0700

    Init project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import Summary from "./Summary"
+
+class App extends PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <Summary/>
+      </div>
+    );
+  }
+}
+
+export default App;
